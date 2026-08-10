bbf50fdbe4f0121d2652d1ac405f4a2548702994
Faine Mesquita <faine.mesquita@leapdev.io>
2026-08-10 12:57:38 +1000

    Update merge-sort.test.js

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,38 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // best case, return if length 1 or 2
+  if (nums.length < 2) {
+    return nums
+  }
+  //1. break into two smaller arrays
+  const length = nums.length;
+  const middle = Math.floor(length/2); //floor rounds up
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+  //2. call mergeSort on left and right
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+  //3. return the merge of left amd right
+  return merge(sortedLeft, sortedRight);
 };
 
-// unit tests
-// do not modify the below code
-test.skip("merge sort", function () {
+//left and right are already sorted!!!
+const merge = (left, right) => {
+  //return one sorted array
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift()); // shift gets the fist item of the array
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return results.concat(left,right) //one side can be empty, but concat in an empty array does nothing
+}
+
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
